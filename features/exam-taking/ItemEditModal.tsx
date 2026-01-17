15a0d5adbedd36f654033cddf23948e2511fc836
Emerson Lucio <eldastito@gmail.com>
2026-01-16 23:31:36 -0300

    fix(build): resolve broken RichTextEditor import in ItemEditModal

--- features/exam-taking/ItemEditModal.tsx
+++ features/exam-taking/ItemEditModal.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { X, Save, AlertCircle } from 'lucide-react';
 import { Item, DifficultyLevel } from '../../types';
-import { RichTextEditor } from '../RichTextEditor';
+import { RichTextEditor } from '../../components/RichTextEditor';
 
 interface ItemEditModalProps {
     item: Item;
