a8bca962e4e02524de7f684adaf0d270be85ce53
Emerson Lucio <eldastito@gmail.com>
2026-03-10 00:47:34 -0300

    Fix: created tablets table, unified ESM vendor bundles, and updated importmap

--- android/app/src/main/assets/public/vendor/zustand.js
+++ android/app/src/main/assets/public/vendor/zustand.js
@@ -1,1 +1,8 @@
-Not found: /zustand@4.5.5/umd/zustand.production.min.js
+/**
+ * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
+ * Original file: /npm/zustand@5.0.10/esm/index.mjs
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+export*from"/npm/zustand@5.0.10/vanilla/+esm";export*from"/npm/zustand@5.0.10/react/+esm";export default null;
+//# sourceMappingURL=/sm/f35717f2db580126484e3b8106655b207d3577ea4cb7eccc330d69b91125ec7d.map
